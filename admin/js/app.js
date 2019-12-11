e5e5f22768889a4488bd8b67a69ac25b87d9c5e9
mxisrar <55828498+mxisrar@users.noreply.github.com>
2019-12-11 20:16:57 +0530

    validation of image

--- admin/js/app.js
+++ admin/js/app.js
@@ -44,60 +44,85 @@ app.controller("productsController", function ($scope, $http) {
     var productParam = {
         isFeatured: 1
     }
-    
-    $http({ url: "http://localhost:3000/getproducts", data:productParam, method: "POST" }).then(function (res) {
+
+    $http({ url: "http://localhost:3000/getproducts", data: productParam, method: "POST" }).then(function (res) {
         console.log(res);
         if (res.status == 200) {
             $scope.pdata = res.data.data;
         }
     })
     // $scope.rowLimit = 3;
-    
-    
-   
+
+
+
 })
 
 
 app.controller("addproductController", function ($scope, $http) {
 
-        $scope.createBrand = function () {
-            if ( $scope.productForm.$valid) {
-                // var brandData = {
-                //     brandName: $scope.brandName,
-                //     brandDesc: $scope.brandDesc,
-                // }
-                // console.log(brandData);
-                // $http({ url: "http://localhost:5000/submitData", data: brandData, method: "POST" }).then(function (res) {
-                //     console.log("---");
-                //     if (res.status == 200) {
-                //         $scope.pdata = res.data;
-                //     }
-                // })
-            }
-            
-     
+    $scope.createBrand = function () {
+        if ($scope.productForm.$valid) {
+            // var brandData = {
+            //     brandName: $scope.brandName,
+            //     brandDesc: $scope.brandDesc,
+            // }
+            // console.log(brandData);
+            // $http({ url: "http://localhost:5000/submitData", data: brandData, method: "POST" }).then(function (res) {
+            //     console.log("---");
+            //     if (res.status == 200) {
+            //         $scope.pdata = res.data;
+            //     }
+            // })
+        }
+
+
     }
     $scope.rowLimit = 3;
 })
-app.controller("brandsController", function ($scope, $http) {
+app.controller("brandsController", function ($scope, $http, fileUpload) {
     $scope.createBrand = function () {
-
-       
-            if ( $scope.brandForm.$valid) {
-                var brandData = {
-                    brandName: $scope.brandName,
-                    brandDesc: $scope.brandDesc,
-                }
-                console.log(brandData);
-                $http({ url: "http://localhost:5000/submitData", data: brandData, method: "POST" }).then(function (res) {
-                    console.log("---");
-                    if (res.status == 200) {
-                        $scope.pdata = res.data;
-                    }
-                })
-            }
-            
         
+        console.log("++++++++")
+        if ($scope.brandForm.$valid) {
+            var brandData = {
+                brandName: $scope.brandName,
+                brandDesc: $scope.brandDesc,
+            }
+            console.log(brandData);
+
+            var file = $scope.myFile;
+            var validFormats = ['jpg', 'jpeg', 'png'];
+            console.log('file is ');
+            console.log(file.name)
+            console.log(file.name.substr(file.name.lastIndexOf('.') + 1))
+            console.dir(file);
+            var ext = file.name.substr(file.name.lastIndexOf('.') + 1);
+            if (ext == '') return;
+            if (validFormats.indexOf(ext) == -1) {
+                $scope.bFlag = 1
+                $scope.msg = "please select valid type (for eg: 'jpg', 'jpeg', 'png')"
+            } else {
+                if (file.size > 5000) {
+                    $scope.bFlag = 1
+                    $scope.msg = "please select valid type "
+                    alert("image size cannot greater than 5000")
+                } else {
+                    var uploadUrl = "http://localhost:5000/submitData"
+                    fileUpload.uploadFileToUrl(file, uploadUrl);
+                }
+
+            }
+
+
+            // $http({ url: "http://localhost:5000/submitData", data: brandData, method: "POST" }).then(function (res) {
+            //     console.log("---");
+            //     if (res.status == 200) {
+            //         $scope.pdata = res.data;
+            //     }
+            // })
+        }
+
+
     }
     $scope.rowLimit = 3;
 })
@@ -105,19 +130,43 @@ app.controller('brandNavController', function NavCtrl($location, $scope) {
 
 });
 
-app.directive('validFile',function(){
+app.directive('validFile', function () {
     return {
-        require:'ngModel',
-        link:function(scope,el,attrs,ctrl){
+        require: 'ngModel',
+        link: function (scope, el, attrs, ctrl) {
             ctrl.$setValidity('validFile', el.val() != '');
             //change event is fired when file is selected
-            el.bind('change',function(){
+            el.bind('change', function () {
                 ctrl.$setValidity('validFile', el.val() != '');
-                scope.$apply(function(){
+                scope.$apply(function () {
                     ctrl.$setViewValue(el.val());
                     ctrl.$render();
                 });
             });
         }
     }
-})
+}).directive('fileModel', ['$parse', function ($parse) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            var model = $parse(attrs.fileModel);
+            var modelSetter = model.assign;
+            element.bind('change', function () {
+                scope.$apply(function () {
+                    modelSetter(scope, element[0].files[0]);
+                });
+            });
+        }
+    };
+
+}]).service('fileUpload', ['$http', function ($http) {
+    this.uploadFileToUrl = function (file, uploadUrl) {
+        var fd = new FormData();
+        console.log("=====filetourl")
+        fd.append('file', file);
+        $http.post(uploadUrl, fd, {
+            transformRequest: angular.identity,
+            headers: { 'Content-Type': undefined }
+        })
+    }
+}]);
